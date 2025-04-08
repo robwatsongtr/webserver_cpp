832a872aa053e01b97ca71e549ed896a06f00fe8
robwatsongtr <rwatso@gmail.com>
2025-04-08 15:44:23 -0700

    more work on .h

--- webserver.cpp
+++ webserver.cpp
@@ -8,11 +8,21 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-WebServer::Connection::Connection(int client_socket) : client_socket(client_socket) {}
+WebServer::Connection::Connection(int client_socket) : 
+    m_client_socket(client_socket) {}
 
 WebServer::Connection::~Connection() {
     // if check because an invalid socket returns -1 in POSIX
-    if (client_socket >= 0) {
-        close(client_socket);
+    if (m_client_socket >= 0) {
+        close(m_client_socket);
+    } 
+}
+
+WebServer::Server::Server(std::string ip_address, int port) : 
+    m_ip_address(ip_address), m_port(port), m_server_socket(-1) {}
+
+WebServer::Server::~Server() {
+    if (m_server_socket >= 0) {
+        close(m_server_socket);
     } 
 }
